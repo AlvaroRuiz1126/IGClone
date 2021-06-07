c1185e3d1ff6fa09b906ebf672aae3ed7ab1cb34
AlvaroRuiz1126 <cheruiz.1126@gmail.com>
2021-06-07 15:20:02 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import LoginScreen from './pages/auth/LoginScreen';
+
+const InstagramApp = () => {
+  return (
+    <LoginScreen />
+  );
+};
+
+export default InstagramApp;
---
+++ src/pages/auth/LoginScreen.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const LoginScreen = () => {
+    return (
+        <React.Fragment>
+            <main className="container center">
+                <h1>IGClone</h1>
+
+                <section>
+                    <h2>Login</h2>
+                </section>
+            </main>
+        </React.Fragment>
+    );
+};
+
+export default LoginScreen;
